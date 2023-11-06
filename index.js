e9fc72bfb3c758c73b4a8397dcfc91071bf6686f
Md Shakil Hossain <mrshakilhossainofficial@gmail.com>
2023-11-07 03:04:36 +0600

    get data from here to show on client

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,7 +26,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+      await client.connect();
+      
+      const foodCollection = client.db('excessEats').collection('foods');
+      const foodRequestCollection = client.db('excessEats').collection('food_requests');
+
+      app.get('/foods', async (req, res) => {
+          const cursor = foodCollection.find();
+          const result = await cursor.toArray();
+          res.send(result);
+      })
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
