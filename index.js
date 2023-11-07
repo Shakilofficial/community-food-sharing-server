b9c43c6b0611be3f1d60b39326e6201381e85c4f
Md Shakil Hossain <mrshakilhossainofficial@gmail.com>
2023-11-07 15:40:34 +0600

    add food and food-request done

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s65cmwk.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,32 +17,53 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      
-      const foodCollection = client.db('excessEats').collection('foods');
-      const foodRequestCollection = client.db('excessEats').collection('food_requests');
+    await client.connect();
 
-      app.get('/foods', async (req, res) => {
-          const cursor = foodCollection.find();
-          const result = await cursor.toArray();
-          res.send(result);
-      })
+    const foodCollection = client.db("excessEats").collection("foods");
+    const foodRequestCollection = client
+      .db("excessEats")
+      .collection("food_requests");
 
+    app.get("/foods", async (req, res) => {
+      const cursor = foodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    });
 
+    //food add
+    app.post("/foods", async (req, res) => {
+      const addFood = req.body;
+      console.log(addFood);
+      const result = await foodCollection.insertOne(foods);
+      res.send(result);
+    });
 
-
-
+    //food-request
+    app.post("/request/:id", async (req, res) => {
+      const requestFood = req.body;
+      console.log(requestFood);
+      const result = await foodRequestCollection.insertOne(requestFood);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -53,12 +71,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-    res.send('community-food-sharing server is running')
-})
+app.get("/", (req, res) => {
+  res.send("community-food-sharing server is running");
+});
 
 app.listen(port, () => {
-    console.log(`community-food-sharing server is running on port ${port}`);
-})
+  console.log(`community-food-sharing server is running on port ${port}`);
+});
